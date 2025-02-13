355049150396baa67645563068a8aa4926820417
Loppan1 <139924795+Loppan1@users.noreply.github.com>
2025-02-13 11:52:18 +0100

    Sorting

--- src/components/CharacterTable.tsx
+++ src/components/CharacterTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Character } from "../pages/View";
 import CharacterRow from "./CharacterRow";
 
@@ -11,6 +11,18 @@ const CharacterTable: React.FC<CharacterTableProps> = ({
   characters,
   selectedClasses,
 }) => {
+  const [sortBy, setSortBy] = useState<string | null>(null);
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
+
+  const handleSort = (key: string) => {
+    if (sortBy === key) {
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      setSortBy(key);
+      setSortOrder("asc");
+    }
+  };
+
   const filteredCharacters =
     selectedClasses.length > 0
       ? characters.filter((char) =>
@@ -18,11 +30,49 @@ const CharacterTable: React.FC<CharacterTableProps> = ({
         )
       : characters;
 
+  const sortedCharacters = [...filteredCharacters].sort((a, b) => {
+    if (!sortBy) return 0;
+
+    let result = 0;
+    if (sortBy === "class") {
+      result = a.characterClass.localeCompare(b.characterClass);
+    }
+    if (sortBy === "name") {
+      result = a.name.localeCompare(b.name);
+    }
+    if (sortBy === "level") {
+      result = a.level - b.level;
+    }
+
+    return sortOrder === "asc" ? result : -result;
+  });
+
   return (
     <div className="overflow-x-auto">
       <table className="min-w-full border-collapse text-left text-sm text-gray-700">
+        <thead className="bg-gray-100 border border-gray-300">
+          <tr>
+            <th />
+            {["name", "class", "level"].map((key) => (
+              <th
+                key={key}
+                className="text-right px-2 cursor-pointer border border-gray-300"
+                onClick={() => handleSort(key)}
+              >
+                <span
+                  className={`text-lg ${
+                    sortBy === key ? "text-gray-800" : "text-gray-400"
+                  }`}
+                >
+                  {sortBy === key && sortOrder === "asc" ? "⏶" : "⏷"}
+                </span>
+              </th>
+            ))}
+            <th />
+          </tr>
+        </thead>
         <tbody>
-          {filteredCharacters.map((char, index) => (
+          {sortedCharacters.map((char, index) => (
             <CharacterRow key={char.id} char={char} index={index} />
           ))}
         </tbody>

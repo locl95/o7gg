bb33b83b57a8845d3b22865087393b1693942b93
eric lopez <e.4p.bigdata.tid@telefonica.com>
2025-11-21 02:33:19 +0100

    fix undead

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -14,7 +14,7 @@ export const RACE_ICONS: Record<string, string> = {
   Human: "/icons/race/human.webp",
   Orc: "/icons/race/orc.webp",
   "Night Elf": "/icons/race/nightelf.webp",
-  Forsaken: "/icons/forsaken.webp",
+  Undead: "/icons/forsaken.webp",
   Tauren: "/icons/race/tauren.webp",
   Gnome: "/icons/race/gnome.webp",
   Troll: "/icons/race/troll.webp",
